0791ed26c769ed460cabe5ebb43fc0a41bd5cac1
minhkl <minh.luong@nfq.asia>
2021-12-10 17:01:27 +0700

    change last section' background

--- src/pages/home/components/PartnersSection.tsx
+++ src/pages/home/components/PartnersSection.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 
 const PartnersSection = () => (
-  <section className="p-4 py-8 sm:py-20 min-h-screen home-bg-1 home-section-bg">
+  <section className="p-4 py-8 sm:py-20 min-h-screen home-bg-2 home-section-bg">
     <div className="home-content">
       <div className="flex flex-col items-center">
         <h2 className="home-heading mb-12">Developers</h2>
