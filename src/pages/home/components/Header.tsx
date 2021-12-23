0b194bb63e1150043584182960aab9879867752d
minhkl <minh.luong@nfq.asia>
2021-12-23 07:52:31 +0700

    responsive step 4

--- src/pages/home/components/Header.tsx
+++ src/pages/home/components/Header.tsx
@@ -1,12 +1,22 @@
 import { useState } from 'react';
 import Image from 'next/image';
 import { HiMenu } from 'react-icons/hi';
+import useIsMobile from 'utils/hooks/useIsMobile';
 const Header = () => {
+  const isMobile = useIsMobile();
   const [isOpenMenu, setIsOpenMenu] = useState<boolean>(false);
 
   const toggleMenu = () => {
     setIsOpenMenu((current) => !current);
   };
+
+  const handleClickLink = () => {
+    if (isMobile) {
+      setIsOpenMenu(false);
+    }
+  };
+
+  const showMenu = !isMobile || isOpenMenu;
 
   return (
     <div className="flex flex-col md:flex-row md:justify-between md:items-center md:h-24 bg-secondary1 w-full px-8">
@@ -22,40 +32,42 @@ const Header = () => {
         </div>
         <HiMenu className="md:hidden text-main2" onClick={toggleMenu} />
       </div>
-      {isOpenMenu && (
+      {showMenu && (
         <nav className="flex flex-col md:flex-row gap-4 md:gap-9 text-main2">
-          <a className="hover:text-main1" href="#trailer">
+          <a className="hover:text-main1" href="#trailer" onClick={handleClickLink}>
             Trailer
           </a>
-          <a className="hover:text-main1" href="#introduction">
+          <a className="hover:text-main1" href="#introduction" onClick={handleClickLink}>
             Introduction
           </a>
-          <a className="hover:text-main1" href="#game-concept">
+          <a className="hover:text-main1" href="#game-concept" onClick={handleClickLink}>
             Game Concept
           </a>
-          <a className="hover:text-main1" href="#roadmap">
+          <a className="hover:text-main1" href="#roadmap" onClick={handleClickLink}>
             Roadmap
           </a>
-          <a className="hover:text-main1" href="#tokenomic">
+          <a className="hover:text-main1" href="#tokenomic" onClick={handleClickLink}>
             Tokenomic
           </a>
-          <a className="hover:text-main1" href="#team">
+          <a className="hover:text-main1" href="#team" onClick={handleClickLink}>
             Team
           </a>
-          <a className="hover:text-main1" href="#pitch-deck">
+          <a className="hover:text-main1" href="#pitch-deck" onClick={handleClickLink}>
             Pitch Deck
           </a>
-          <a className="hover:text-main1" href="#document">
+          <a className="hover:text-main1" href="#document" onClick={handleClickLink}>
             Document
           </a>
         </nav>
       )}
-      <button
-        type="button"
-        className="hidden md:block bg-main2 hover:bg-main1 h-11 w-48 text-white text-xl font-soup"
-      >
-        MARKETPLACE
-      </button>
+      {showMenu && (
+        <button
+          type="button"
+          className="md:block bg-main2 hover:bg-main1 h-11 w-full md:w-48 text-white text-xl font-soup my-8 md:my-0"
+        >
+          MARKETPLACE
+        </button>
+      )}
     </div>
   );
 };
--- src/pages/home/index.page.tsx
+++ src/pages/home/index.page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
         <title>Lost Dungeon</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Headroom disable={isMobile}>
+      <Headroom>
         <Header />
       </Headroom>
 
